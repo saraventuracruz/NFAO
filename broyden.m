cea99dbbba349ddb932ea78501a273b62418b405
Sara Cruz <saraventuracruz@gmail.com>
2019-12-15 23:40:35 +0000

    added comments

--- broyden.m
+++ broyden.m
@@ -1,12 +1,12 @@
 % Broyden's method to find x s.t. F(x) = 0
 function [xk,iter, err] = broyden(F,x0, A0, x, eps)
 
-xk = x0;
-Ak = A0;
-Fk = subs(F, x, xk);
-ek = double(norm(Fk));
-iter = 0;
-err = ek;
+xk = x0; % initialize xk
+Ak = A0; % initialize Ak
+Fk = subs(F, x, xk); % compute F0
+ek = double(norm(Fk)); % compute current error
+iter = 0; % initalize iterations' counter
+err = ek; % save the current error for output
 while (ek > eps)
     
     sk = -Ak\Fk; % solve the linear system Ak*sk = -Fk
@@ -14,9 +14,9 @@ while (ek > eps)
     yk = subs(F, x, xk) - Fk;
     Fk = subs(F, x, xk); % update Fk value
     Ak = Ak + 1/(transpose(sk)*sk)*(yk-Ak*sk)*transpose(sk); % Broyden's update
-    ek = double(norm(Fk));
-    err = [err, ek];
-    iter = iter + 1;
+    ek = double(norm(Fk)); % compute current error
+    err = [err, ek]; % save the current error for output
+    iter = iter + 1; % update the iterations' counter
     if iter > 200
         break
     end
--- newton.m
+++ newton.m
@@ -1,21 +1,23 @@
 % Newton's method to find x s.t. F(x) = 0
 function [xk,iter, err] = newton (F,x0, x, eps)
 
-xk = x0;
-Fk = subs(F,x,xk);
-jac = jacobian(F, x);
-Jk = subs(jac, x, xk);
-ek = double(norm(Fk));
-iter = 0;
-err = ek;
+xk = x0; % initialize xk
+jac = jacobian(F, x); % define the jacobian of F
+Fk = double(subs(F, x, xk)); % compute the first Fk
+Jk = subs(jac, x, xk); % evaluate the jacobian on x0
+ek = double(norm(Fk)); % update the error
+err = ek; % % save the first error for output
+iter = 0; % initialize iterations' counter
 while(ek > eps)
     
-    xk = xk-(Jk\Fk);
-    Fk = double(subs(F, x, xk));
-    Jk = subs(jac, x, xk);
-    ek = double(norm(Fk));
-    err = [err, ek];
-    iter = iter + 1;
+    
+    
+    xk = xk-(Jk\Fk); % update xk
+    Fk = double(subs(F, x, xk)); % compute current Fk
+    Jk = subs(jac, x, xk); % compute current jacobian
+    ek = double(norm(Fk)); % update the error
+    err = [err, ek]; % save the current error for output
+    iter = iter + 1; % update the iterations' counter
     if iter > 200
         break
     end
