6a85a9e13f8ff5f39fa24a5e34702b2594001515
Sara Cruz <saraventuracruz@gmail.com>
2019-12-15 23:33:13 +0000

    output update

--- SR1.m
+++ SR1.m
@@ -1,5 +1,5 @@
 % Symetric rank 1 quasi-Newton method
-function [xk,iter,err] = SR1 (f,gradf,H0, x, x0, eps)
+function [Xk, xk,iter,err] = SR1 (f,gradf,H0, x, x0, eps)
     
 xk = x0;
 Hk = H0;
@@ -8,9 +8,11 @@ iter = 0;
 gk = double(subs(gradf,x,xk));
 ek = norm(gk);
 err = [];
+Xk = x0;
 while(ek>eps)
     sk     = -Bk*gk;
     xk     = sk' + xk;
+    Xk     = [Xk;xk];
     gk_new = double(subs(gradf,x,xk)); 
     ek     = norm(gk_new);
     err    = [err,ek];
--- newton.m
+++ newton.m
@@ -1,20 +1,24 @@
-function [xk,iter, err] = newton (F,x0, var_x, var_lambda, eps)
+% Newton's method to find x s.t. F(x) = 0
+function [xk,iter, err] = newton (F,x0, x, eps)
 
 xk = x0;
-Fk = subs(F,[transpose(var_x) transpose(var_lambda)],xk)
-jac = jacobian(F, [transpose(var_x) transpose(var_lambda)]);
-Jk = subs(jac, [transpose(var_x) transpose(var_lambda)], xk)
+Fk = subs(F,x,xk);
+jac = jacobian(F, x);
+Jk = subs(jac, x, xk);
 ek = double(norm(Fk));
 iter = 0;
 err = ek;
 while(ek > eps)
     
-    xk = xk-(Jk\Fk)';
-    Fk = double(subs(F, [transpose(var_x) transpose(var_lambda)], xk));
-    Jk = subs(jac, [transpose(var_x) transpose(var_lambda)], xk);
+    xk = xk-(Jk\Fk);
+    Fk = double(subs(F, x, xk));
+    Jk = subs(jac, x, xk);
     ek = double(norm(Fk));
     err = [err, ek];
     iter = iter + 1;
+    if iter > 200
+        break
+    end
     
 end
 
